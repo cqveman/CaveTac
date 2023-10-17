a31b60272f6ff9372796ce038c2217046543e988
cqveman <104222495+cqveman@users.noreply.github.com>
2023-10-17 08:24:13 +0300

    Updating 2D array to be more flexible
    The first for-loop will go over the rows first and add'em to the board array, then the second will go over the columns and print the symbol in the cell
    
    Added an updateSymbol method in the createPlayer object

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const gameBoard = (() => {
-	const gameboard = [
-		["X", "O", "O"],
-		["O", "X", "X"],
-		["X", "O", "X"],
-	];
+const gameboard = (() => {
+	const rows = 3;
+	const columns = 3;
+	const board = [];
 
-	const gameboardInfo = () => gameboard;
+	for (let i = 0; i < rows; i++) {
+		board[i] = [];
+		for (let j = 0; j < columns; j++) {
+			board[i].push(createPlayer("Player", "x").printSymbol)
+		}
+	}
+
+	const gameboardInfo = () => board;
 	return { gameboardInfo: gameboardInfo };
 })();
 
 function createPlayer(name, symbol = "", move) {
 	const printName = () => `Player name is ${name}`;
 	const printSymbol = () => symbol.toUpperCase();
+	const updateSymbol = (newSymbol) => symbol = newSymbol;
 
-	return { printName, printSymbol };
+	return { printName, printSymbol, updateSymbol};
 }
 
 const cqveman = createPlayer("cqveman", "x");
 
 const gameControl = (() => {
-    const winner = () => winner;
-    const isGameOver = () => isGameOver;
+	const winner = () => winner;
+	const isGameOver = () => isGameOver;
 })();
