934b668d2b845e75a57864c5d14d807300c7745c
cqveman <104222495+cqveman@users.noreply.github.com>
2023-10-17 23:20:44 +0300

    Updating the whole logic idgaf

--- script.js
+++ script.js
@@ -1,45 +1,101 @@
 const gameboard = (() => {
-	const rows = 3;
-	const columns = 3;
-	const board = [];
-
-	const X = createPlayer("Player", "x").printSymbol();
-	const O = createPlayer("Player", "o").printSymbol();
-	let currentPlayer = X;
-
-	for (let i = 0; i < rows; i++) {
-		board[i] = [];
-		for (let j = 0; j < columns; j++) {
-			board[i][j] = "";
-		}
-	}
-
-	const getBoard = () => board;
-
-	const markOnBoard = (row, col) => {
-		if (board[row][col] === "") {
-			board[row][col] = currentPlayer;
-
-			currentPlayer = currentPlayer === "X" ? "O" : "X";
-		} else {
-			alert("That spot is already taken. Please choose another one.");
-		}
+	let board = [];
+  
+	const init = () => {
+	  board = [null, null, null, null, null, null, null, null, null];
 	};
-
-	return { getBoard, markBoard: markOnBoard,  };
-})();
-
-function createPlayer(name, symbol = "", move) {
-	const printName = () => `Player name is ${name}`;
-	const printSymbol = () => symbol.toUpperCase();
-	const updateSymbol = (newSymbol) => (symbol = newSymbol);
-
-	return { printName, printSymbol, updateSymbol };
-}
-
-// const cqveman = createPlayer("cqveman", "x");
-
-const gameControl = (() => {
-	const winner = () => winner;
-	const isGameOver = () => isGameOver;
-})();
+  
+	const getBoard = () => board;
+  
+	const reset = () => init();
+  
+	const makeMove = (player, position) => {
+	  if (board[position] === null) {
+		board[position] = player;
+	  } else {
+		alert("That spot is already taken. Please choose another one.");
+	  }
+	};
+  
+	const displayTable = () => {
+	  console.log(board);
+	};
+  
+	return { init, getBoard, reset, makeMove, displayTable };
+  })();
+  
+  function createPlayer(symbol = "") {
+	const getSymbol = () => symbol.toUpperCase();
+  
+	return { getSymbol };
+  }
+  
+  const gameController = (() => {
+	const player1 = createPlayer("X");
+	const player2 = createPlayer("O");
+  
+	let currentPlayer = null;
+	let gameOver = false;
+  
+	const startGame = () => {
+	  gameboard.init();
+	  currentPlayer = player1.getSymbol();
+	  gameOver = false;
+	  playGame();
+	};
+  
+	const playGame = () => {
+	  while (!gameOver) {
+		console.log("Current player:", currentPlayer);
+  
+		gameboard.getBoard();
+		const position = prompt(
+		  `Player ${currentPlayer}, enter a position (0-8): `
+		);
+		gameboard.displayTable();
+		gameboard.makeMove(currentPlayer, parseInt(position, 10));
+		checkGameStatus();
+		currentPlayer =
+		  currentPlayer === player1.getSymbol()
+			? player2.getSymbol()
+			: player1.getSymbol();
+	  }
+	};
+	const checkGameStatus = () => {
+	  const board = gameboard.getBoard();
+	  const winningCombinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	  ];
+  
+	  let isDraw = true;
+  
+	  for (let i = 0; i < winningCombinations.length; i++) {
+		const [a, b, c] = winningCombinations[i];
+		if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+		  console.log(`Player ${board[a]} wins!`);
+		  gameOver = true;
+		  break;
+		}
+	  }
+  
+	  for (let i = 0; i < board.length; i++) {
+		if (board[i] === null) {
+		  isDraw = false;
+		  break;
+		}
+	  }
+	  if (isDraw) {
+		console.log("It's a draw!");
+		gameOver = true;
+	  }
+	};
+  
+	return { startGame };
+  })();
