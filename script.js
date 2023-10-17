59a913756e5567d65a6999867efbb87b7dab038c
cqveman <104222495+cqveman@users.noreply.github.com>
2023-10-17 08:56:59 +0300

    Adding the ability to mark symbols on the board
    The markOnBoard() checks if the current array position is available then it will set it to currentPlayer, otherwise it'll alert the user

--- script.js
+++ script.js
@@ -3,26 +3,41 @@ const gameboard = (() => {
 	const columns = 3;
 	const board = [];
 
+	const X = createPlayer("Player", "x").printSymbol;
+	const O = createPlayer("Player", "o").printSymbol;
+	let currentPlayer = X;
+
 	for (let i = 0; i < rows; i++) {
 		board[i] = [];
 		for (let j = 0; j < columns; j++) {
-			board[i].push(createPlayer("Player", "x").printSymbol)
+			board[i].push(createPlayer("Player", "x").printSymbol);
 		}
 	}
 
-	const gameboardInfo = () => board;
-	return { gameboardInfo: gameboardInfo };
+	const getBoard = () => board;
+
+	const markOnBoard = (row, col) => {
+		if (board[row][col] === "") {
+			board[row][col] = currentPlayer;
+
+			currentPlayer = currentPlayer === "X" ? "O" : "X";
+		} else {
+			alert("That spot is already taken. Please choose another one.");
+		}
+	};
+
+	return { getBoard, markBoard: markOnBoard,  };
 })();
 
 function createPlayer(name, symbol = "", move) {
 	const printName = () => `Player name is ${name}`;
 	const printSymbol = () => symbol.toUpperCase();
-	const updateSymbol = (newSymbol) => symbol = newSymbol;
+	const updateSymbol = (newSymbol) => (symbol = newSymbol);
 
-	return { printName, printSymbol, updateSymbol};
+	return { printName, printSymbol, updateSymbol };
 }
 
-const cqveman = createPlayer("cqveman", "x");
+// const cqveman = createPlayer("cqveman", "x");
 
 const gameControl = (() => {
 	const winner = () => winner;
